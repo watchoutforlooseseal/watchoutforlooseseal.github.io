e077a73d9003d219763fe64ed676bcf2fe0748f5
watchoutforlooseseal <106763055+watchoutforlooseseal@users.noreply.github.com>
2024-11-26 15:19:09 -0800

    html updates

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const menuToggle = document.getElementById('mobile-menu');
+    const navLinks = document.getElementById('nav-links');
+
+    menuToggle.addEventListener('click', function() {
+        navLinks.classList.toggle('showing');
+    });
+});
